074e9e8421e608b60cb5e44d2b9267e890f7ec8d
rsolanodev <rubensoljim@gmail.com>
2026-02-12 15:12:59 +0100

    [Added] Direction conditions more readable and map next direction

--- passporter/main.py
+++ passporter/main.py
@@ -8,6 +8,18 @@ class Direction(StrEnum):
     EAST = "east"
     WEST = "west"
 
+    def is_north(self) -> bool:
+        return self == Direction.NORTH
+
+    def is_south(self) -> bool:
+        return self == Direction.SOUTH
+
+    def is_east(self) -> bool:
+        return self == Direction.EAST
+
+    def is_west(self) -> bool:
+        return self == Direction.WEST
+
 
 class Movement(StrEnum):
     FORWARD = "forward"
@@ -23,72 +35,56 @@ class MarsRover:
     direction: Direction
 
     def forward(self) -> None:
-        if self.direction == Direction.NORTH:
+        if self.direction.is_north():
             self.y = self.y + 1
             return
 
-        if self.direction == Direction.SOUTH:
+        if self.direction.is_south():
             self.y = self.y - 1
             return
 
-        if self.direction == Direction.EAST:
+        if self.direction.is_east():
             self.x = self.x + 1
             return
 
-        if self.direction == Direction.WEST:
+        if self.direction.is_west():
             self.x = self.x - 1
             return
 
     def backward(self) -> None:
-        if self.direction == Direction.NORTH:
+        if self.direction.is_north():
             self.y = self.y - 1
             return
 
-        if self.direction == Direction.SOUTH:
+        if self.direction.is_south():
             self.y = self.y + 1
             return
 
-        if self.direction == Direction.EAST:
+        if self.direction.is_east():
             self.x = self.x - 1
             return
 
-        if self.direction == Direction.WEST:
+        if self.direction.is_west():
             self.x = self.x + 1
             return
 
     def left(self) -> None:
-        if self.direction == Direction.NORTH:
-            self.direction = Direction.WEST
-            return
-
-        if self.direction == Direction.EAST:
-            self.direction = Direction.NORTH
-            return
-
-        if self.direction == Direction.SOUTH:
-            self.direction = Direction.EAST
-            return
-
-        if self.direction == Direction.WEST:
-            self.direction = Direction.SOUTH
-            return
+        map_next_direction = {
+            Direction.NORTH: Direction.WEST,
+            Direction.EAST: Direction.NORTH,
+            Direction.SOUTH: Direction.EAST,
+            Direction.WEST: Direction.SOUTH,
+        }
+        self.direction = map_next_direction[self.direction]
 
     def right(self) -> None:
-        if self.direction == Direction.NORTH:
-            self.direction = Direction.EAST
-            return
-
-        if self.direction == Direction.EAST:
-            self.direction = Direction.SOUTH
-            return
-
-        if self.direction == Direction.SOUTH:
-            self.direction = Direction.WEST
-            return
-
-        if self.direction == Direction.WEST:
-            self.direction = Direction.NORTH
-            return
+        map_next_direction = {
+            Direction.NORTH: Direction.EAST,
+            Direction.EAST: Direction.SOUTH,
+            Direction.SOUTH: Direction.WEST,
+            Direction.WEST: Direction.NORTH,
+        }
+        self.direction = map_next_direction[self.direction]
 
     def move(self, movements: list[Movement]) -> None:
         for movement in movements:
