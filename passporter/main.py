b88c674fe8f4be8aa1e2dc83b0a1b3a59d82ac6f
rsolanodev <rubensoljim@gmail.com>
2026-02-12 13:56:26 +0100

    [Added] Mars rover movements

--- passporter/main.py
+++ passporter/main.py
@@ -1,7 +1,90 @@
-def start():
-    raise NotImplementedError("Method not implemented.")
+from dataclasses import dataclass
+from enum import StrEnum
 
 
-if __name__ == "__main__":
-    start()
+class Direction(StrEnum):
+    N = "N"
+    S = "S"
+    E = "E"
+    W = "W"
 
+
+class RotateOptions(StrEnum):
+    LEFT = "left"
+    RIGHT = "right"
+
+
+@dataclass
+class MarsRover:
+    x: int
+    y: int
+    direction: Direction
+
+    def forward(self) -> None:
+        if self.direction == Direction.N:
+            self.y = self.y + 1
+            return
+
+        if self.direction == Direction.S:
+            self.y = self.y - 1
+            return
+
+        if self.direction == Direction.E:
+            self.x = self.x + 1
+            return
+
+        if self.direction == Direction.W:
+            self.x = self.x - 1
+            return
+
+    def backward(self) -> None:
+        if self.direction == Direction.N:
+            self.y = self.y - 1
+            return
+
+        if self.direction == Direction.S:
+            self.y = self.y + 1
+            return
+
+        if self.direction == Direction.E:
+            self.x = self.x - 1
+            return
+
+        if self.direction == Direction.W:
+            self.x = self.x + 1
+            return
+
+    def rotate(self, to: RotateOptions) -> None:
+        if to == RotateOptions.LEFT:
+            if self.direction == Direction.N:
+                self.direction = Direction.W
+                return
+
+            if self.direction == Direction.E:
+                self.direction = Direction.N
+                return
+
+            if self.direction == Direction.S:
+                self.direction = Direction.E
+                return
+
+            if self.direction == Direction.W:
+                self.direction = Direction.S
+                return
+
+        if to == RotateOptions.RIGHT:
+            if self.direction == Direction.N:
+                self.direction = Direction.E
+                return
+
+            if self.direction == Direction.E:
+                self.direction = Direction.S
+                return
+
+            if self.direction == Direction.S:
+                self.direction = Direction.W
+                return
+
+            if self.direction == Direction.W:
+                self.direction = Direction.N
+                return
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,85 @@
 import pytest
+from passporter.main import Direction, MarsRover, RotateOptions
 
-from passporter.main import start
 
+class TestMarsRover:
+    def test_initial_mars_rover(self) -> None:
+        mars_rover = MarsRover(x=0, y=0, direction=Direction.N)
 
-def test_main_start():
-    with pytest.raises(NotImplementedError):
-        start()
+        assert mars_rover.x == 0
+        assert mars_rover.y == 0
+        assert mars_rover.direction == "N"
+
+    @pytest.mark.parametrize(
+        "direction, expected_y_position, expected_x_position",
+        [
+            (Direction.N, 1, 0),
+            (Direction.S, -1, 0),
+            (Direction.E, 0, 1),
+            (Direction.W, 0, -1),
+        ],
+    )
+    def test_moves_rover_to_forward(
+        self, direction: Direction, expected_y_position: int, expected_x_position: int
+    ) -> None:
+        mars_rover = MarsRover(x=0, y=0, direction=direction)
+
+        mars_rover.forward()
+
+        assert mars_rover.y == expected_y_position
+        assert mars_rover.x == expected_x_position
+
+    @pytest.mark.parametrize(
+        "direction, expected_y_position, expected_x_position",
+        [
+            (Direction.N, -1, 0),
+            (Direction.S, 1, 0),
+            (Direction.E, 0, -1),
+            (Direction.W, 0, 1),
+        ],
+    )
+    def test_moves_rover_to_backward(
+        self, direction: Direction, expected_y_position: int, expected_x_position: int
+    ) -> None:
+        mars_rover = MarsRover(x=0, y=0, direction=direction)
+
+        mars_rover.backward()
+
+        assert mars_rover.y == expected_y_position
+        assert mars_rover.x == expected_x_position
+
+    @pytest.mark.parametrize(
+        "direction, expected_direction",
+        [
+            (Direction.N, Direction.W),
+            (Direction.E, Direction.N),
+            (Direction.S, Direction.E),
+            (Direction.W, Direction.S),
+        ],
+    )
+    def test_rotate_rover_to_left(
+        self, direction: Direction, expected_direction: Direction
+    ) -> None:
+        mars_rover = MarsRover(x=0, y=0, direction=direction)
+
+        mars_rover.rotate(to=RotateOptions.LEFT)
+
+        assert mars_rover.direction == expected_direction
+
+    @pytest.mark.parametrize(
+        "direction, expected_direction",
+        [
+            (Direction.N, Direction.E),
+            (Direction.E, Direction.S),
+            (Direction.S, Direction.W),
+            (Direction.W, Direction.N),
+        ],
+    )
+    def test_rotate_rover_to_right(
+        self, direction: Direction, expected_direction: Direction
+    ) -> None:
+        mars_rover = MarsRover(x=0, y=0, direction=direction)
+
+        mars_rover.rotate(to=RotateOptions.RIGHT)
+
+        assert mars_rover.direction == expected_direction
